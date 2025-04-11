676f05e765e70e177ccb538db4a312bd6bd35b65
Piotr Pęgiel <piotr.pegiel@bimodal-solutions.com>
2025-04-11 14:12:41 +0200

    Added category list page

--- pwa/src/App.tsx
+++ pwa/src/App.tsx
@@ -1,6 +1,7 @@
 import React, { JSX } from "react";
 import Login from "./components/auth/login";
 import Register from "./components/auth/register";
+import Categories from "./components/categories";
 
 import Header from "./components/header";
 import Home from "./components/home";
@@ -22,6 +23,7 @@ function App() {
     { path: "/register", element: <Register /> },
     { path: "/home", element: <ProtectedRoute element={<Home />} /> },
     { path: "/upload", element: <ProtectedRoute element={<Upload />} /> },
+    { path: "/categories", element: <ProtectedRoute element={<Categories />} /> },
   ];
 
   let routesElement = useRoutes(routesArray);
