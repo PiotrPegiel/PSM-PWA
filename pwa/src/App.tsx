2703adc8fd70d83f314ff8d98587a7e04ea9469e
MateuszKubaty <mateuszkubaty8@gmail.com>
2025-04-12 21:27:57 +0200

    fixed overflow

--- pwa/src/App.tsx
+++ pwa/src/App.tsx
@@ -53,7 +53,7 @@ function App() {
   return (
     <>
       {userLoggedIn && <Header />}
-      <div className="w-full h-screen flex flex-col">{routesElement}</div>
+      <div className="w-full flex flex-col pb-6">{routesElement}</div>
     </>
   );
 }
--- pwa/src/components/home/index.tsx
+++ pwa/src/components/home/index.tsx
@@ -96,7 +96,7 @@ const Home: React.FC = () => {
     }
 
     return (
-        <div className="container mx-auto text-center px-4 p-12 font-inter">
+        <div className="container mx-auto text-center px-4 p-12 font-inter min-h-screen overflow-y-auto">
             <h1 className="text-2xl font-semibold mb-6">Current Reservations</h1>
             <button
                 onClick={() => navigate('/new-reservation')}
